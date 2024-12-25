59a0a6b0401df3c6d735dcf5ed86166faa4136c3
Thomas <117567137+loopfinito@users.noreply.github.com>
2024-12-25 18:45:12 +0100

    Add a JSON serialization

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import urllib.request
 import os.path
 import tiktoken
+import json
 
 # Import sample text and read it
 
@@ -16,6 +17,18 @@ if not os.path.isfile(file_path) :
   urllib.request.urlretrieve(url, file_path) 
 
 # Encode sample file
-encoding = tiktoken.encoding_for_model("gpt-4o")
-num_tokens = len(encoding.encode(txt))
+encoding = tiktoken.encoding_for_model("gpt2")
+
+enc_path = "encoded.json"
+enc = ()
+if not os.path.isfile(enc_path):
+  enc = encoding.encode(txt)
+  with open(enc_path, 'w+') as enc_f:
+    enc_f.write(json.dumps(enc))
+    enc_f.close()
+else:
+  with open(enc_path) as enc_f:
+    enc = json.loads(enc_f.read())
+
+num_tokens = len(enc)
 print(f'Number of tokens : {num_tokens} ')
