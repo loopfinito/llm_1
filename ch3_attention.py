16dcbcea17f807935fe5ab1f8a6ae42923cafb90
Thomas <117567137+loopfinito@users.noreply.github.com>
2025-01-01 13:31:30 +0100

    Add v3 with causal attention

--- ch3_attention.py
+++ ch3_attention.py
@@ -1,6 +1,24 @@
 import torch
 import torch.nn as nn
 
+class SelfAttention_v1(nn.Module):
+    def __init__(self, d_in, d_out):
+        super().__init__()
+        self.W_query = nn.Parameter(torch.rand(d_in, d_out))
+        self.W_key = nn.Parameter(torch.rand(d_in, d_out))
+        self.W_value = nn.Parameter(torch.rand(d_in, d_out))
+
+    def forward(self, x):
+        queries = x @ self.W_query
+        keys = x @ self.W_key
+        values = x @ self.W_value
+        attn_scores = queries @ keys.T
+        attn_weights = torch.softmax(
+            attn_scores / keys.shape[-1]**0.5, dim=-1
+        )
+        context_vec = attn_weights @ values
+        return context_vec
+
 class SelfAttention_v2(nn.Module):
     def __init__(self, d_in, d_out, qkv_bias=False):
         super().__init__()
@@ -18,7 +36,32 @@ class SelfAttention_v2(nn.Module):
         )
         context_vec = attn_weights @ values
         return context_vec
+
+# Causal attention mechanism is added
+class SelfAttention_v3(nn.Module):
+    def __init__(self, d_in, d_out, qkv_bias=False):
+        super().__init__()
+        self.W_query = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_key = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_value = nn.Linear(d_in, d_out, bias=qkv_bias)
         
+
+    def forward(self, x):
+        queries = self.W_query(x)
+        keys = self.W_key(x)
+        values = self.W_value(x)
+        attn_scores = queries @ keys.T
+
+        mask = torch.triu(torch.ones(attn_scores.shape), diagonal = 1)
+        masked = attn_scores.masked_fill(mask.bool(), -torch.inf)
+        print("masked : \n", masked)
+
+        attn_weights = torch.softmax(
+            masked / keys.shape[-1]**0.5, dim=-1
+        )
+        print("Attention weights with causal attention \n : ", attn_weights)
+        context_vec = attn_weights @ values
+        return context_vec  
 
 
 inputs = torch.tensor(
@@ -46,4 +89,17 @@ print("Context shape : {}".format(context.shape))
 # use class SelfAttention
 torch.manual_seed(789)
 sa_v2 = SelfAttention_v2(3,2)
-print("forward self action class: \n", sa_v2(inputs))
+print("forward self action class v2: \n", sa_v2(inputs))
+
+# Exercise 3.1
+torch.manual_seed(123)
+sa_v1 = SelfAttention_v1(3,2)
+sa_v1.W_key.data = sa_v2.W_key.weight.T
+sa_v1.W_value.data = sa_v2.W_value.weight.T
+sa_v1.W_query.data = sa_v2.W_query.weight.T
+print("forward self action class v1 : \n", sa_v1(inputs))
+
+# Causal attention
+torch.manual_seed(789)
+sa_v3 = SelfAttention_v3(3,2)
+print("forward self action class v3: \n", sa_v3(inputs))
