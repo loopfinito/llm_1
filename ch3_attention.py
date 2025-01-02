2b4ef69253753c4553fb183d92622c1da81d1c04
Thomas <117567137+loopfinito@users.noreply.github.com>
2025-01-02 18:24:10 +0100

    Add untested MultiHeadAttention class

--- ch3_attention.py
+++ ch3_attention.py
@@ -108,6 +108,55 @@ class MultiHeadAttentionWrapper(nn.Module):
     def forward(self, x):
         return torch.cat([head(x) for head in self.heads], dim=-1)
 
+class MultiHeadAttenion(nn.Module):
+    def __init__(self, d_in, d_out, context_length,
+                dropout, num_heads, qkv_bias=False):
+        super().__init__()
+        self.W_query = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_key = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_value = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.d_out = d_out // num_heads
+        self.num_heads = num_heads
+        self.head_dim = d_out
+        self.out_proj = nn.Linear(d_out, d_out)
+        self.dropout = dropout
+        self.register_buffer(
+            "mask",
+            torch.triu(torch.ones(context_length, context_length), 
+            diagonal=-1)
+        )
+
+    def forward(self, x):
+        b, num_tokens, d_in = x.shape
+        keys = self.W_key(x)
+        queries = self.W_query(x)
+        values = self.W_value(x)
+        
+        # add a new dimension for heads
+        keys = keys.view(b, num_tokens, self.num_heads, self.head_dim)
+        values = values.view(b, num_tokens, self.num_heads, self.head_dim)
+        queries = queries.view(b, num_tokens, self.num_heads, self.head_dim)
+        
+        # num_heads beforce num_tokens
+        keys = keys.transpose(1,2)
+        queries = queries.transpose(1,2)
+        values = values.transpose(1,2)
+
+        attn_scores = queries @ keys.transpose(2,3)
+        mask_bool = self.mask.bool()[:num_tokens, :num_tokens]
+        attn_scores.masked_fill_(mask_bool, -torch.inf)
+        attn_weights = torch.softmax(
+            attn_scores / keys.shape(-1)**0.5, dim=-1)
+        attn_weights = self.dropout(attn_weights)
+
+        context_vec = (attn_weights @ values).transpose(1,2)
+        
+        context_vec = context_vec.contiguous().view(
+            b, num_tokens, self.d_out
+        )
+        context_vec = self.out_proj(context_vec)
+        return context_vec
+
 
 inputs = torch.tensor(
     [[0.43, 0.15, 0.89],
@@ -155,19 +204,29 @@ d_in = 3
 d_out = 2
 batch = torch.stack((inputs, inputs), dim=0)
 #print("Shape after stacking inputs : ", batch.shape)
-context_length = batch.shape[1]
-torch.manual_seed(123)
-ca = CausalAttention(d_in,d_out, context_length, 0.0)
-context_vecs = ca(batch)
+##context_length = batch.shape[1]
+##torch.manual_seed(123)
+##ca = CausalAttention(d_in,d_out, context_length, 0.0)
+##context_vecs = ca(batch)
 #print("context_vecs.shape :", context_vecs.shape)
 
 # Multi-head
-torch.manual_seed = 123
+torch.manual_seed = 789
 context_length = batch.shape[1]
 d_in, d_out = 3, 2
 mha = MultiHeadAttentionWrapper(
-    d_in, d_out, context_length, 0.0, num_heads=2
+    d_in, d_out, context_length, 0.0, 2
 )
-mah.context_vecs = mha(batch)
+context_vecs = mha(batch)
 print("context_vecs : \n", context_vecs)
 print("context_vecs.shape : \n", context_vecs.shape)
+
+# Exercice 3.2
+# d_out set to 1 to have only two-dimensional vectors
+d_in, d_out = 3, 1
+mha = MultiHeadAttentionWrapper(
+    d_in, d_out, context_length, 0.0, 2
+)
+context_vecs = mha(batch)
+print("context_vecs 3.2 : \n", context_vecs)
+print("context_vecs.shape 3.2 : \n", context_vecs.shape)
